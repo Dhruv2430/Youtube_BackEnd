3f872a50eb3b03b5c97c9c71ad85306e865d9030
Dhruv2430 <dhruvpanchal897@gmail.com>
2025-03-07 23:27:30 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB(); 
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000,() => {
+        console.log(`server is runing ${process.env.PORT}`);
+    })
+    app.on("error",(error)=>{
+        console.error("error",error);
+        
+    })
+})
+.catch((err) => {
+    console.error("not connect",err)
+})
